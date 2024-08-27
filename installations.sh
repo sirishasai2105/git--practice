66d32fccd332bbbfbd9fd480797b20e23001d635
siri123 <saisirishayarra692@gmail.com>
2024-08-27 14:32:13 +0530

    Git Practice

--- installations.sh
+++ installations.sh
@@ -2,20 +2,20 @@
 
 USER_ID=$(id -u)
 
-if ($USER_ID -ne 0)
+if [ $USER_ID -ne 0 ]
 then
     echo "Please take root access to run the packages"
     exit 1
 fi
 
 dnf list installed git
-if[ $? -eq 0 ]
+if [ $? -eq 0 ]
 then
     echo "GIT is already installed ,NOthing to do"
 else
     echo "GIT is not installed .Going to install"
     dnf install git -y
-    if [ $? -eq 0]
+    if [ $? -eq 0 ]
     then 
         echo "GIT installtion is SUCCESS"
     else
