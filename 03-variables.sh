5e4277be662ca70117aa30c32a7d299cc17e25d2
siri123 <saisirishayarra692@gmail.com>
2024-08-27 13:43:29 +0530

    Git Practice

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-USERNAME_1=$1
-USERNAME_2=$2
+# USERNAME_1=$1
+# USERNAME_2=$2
 
-echo "First username is $USERNAME_1"
-echo "Second username is $USERNAME_2"
+# echo "First username is $USERNAME_1"
+# echo "Second username is $USERNAME_2"
+
+fruits =( "apple","banana","grapes")
+echo "First fruit is ${fruits[0]}"
