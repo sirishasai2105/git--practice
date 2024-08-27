b275fe2481afeabd809c9eb976fd59a4391673c3
siri123 <saisirishayarra692@gmail.com>
2024-08-27 14:41:23 +0530

    Git Practice

---
+++ 08-gitsql_instal.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+USER_ID=$(id -u)
+
+if [ USER_ID -ne 0 ]
+then 
+    echo "Get root access and then continue"
+fi
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]
+then
+    echo "MYSQl is already installed , Nothing to do"
+else
+    echo "MYSQl is not installed , Ready to install"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "MYSQL installation is SUCCESS"
+    else
+        echo "MYSQL installation is FAILED"
+        exit 1
+    fi
+fi
