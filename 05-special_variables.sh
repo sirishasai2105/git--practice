db0edc6dfcf1503ccd8b562cf91990e922dc9020
siri123 <saisirishayarra692@gmail.com>
2024-08-27 14:12:42 +0530

    Git Practice

--- 05-special_variables.sh
+++ 05-special_variables.sh
@@ -2,3 +2,8 @@
 
 echo "Printing all characters or variables or arguments $@"
 echo "Printing how many arguments passes into the script $#"
+echo "Getting the script name $0"
+echo "Printing current working directory $PWD"
+echo "Printing process instance id $$"
+echo  "current home directory is $HOME"
+echo "process id of background command $!"
