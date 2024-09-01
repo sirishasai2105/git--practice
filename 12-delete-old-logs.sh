50702ad9bffc523c1bd6e3b801321d731d67305b
siri123 <saisirishayarra692@gmail.com>
2024-09-01 18:18:09 +0530

    Deleting old log files

---
+++ 12-delete-old-logs.sh
@@ -0,0 +1,9 @@
+SOURCE_DIR =/home/ec2-user/logs
+
+if [ -d SOURCE_DIR ]
+then
+    echo "Source directory exists: $SOURCE_DIR"
+else
+    echo "Source directory doesn't exists: $SOURCE_DIR"
+    exit 1
+fi
