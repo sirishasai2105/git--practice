de8b9aa5c637f820689802850fabdd341edcefed
siri123 <saisirishayarra692@gmail.com>
2024-09-01 18:33:15 +0530

    shell

--- 12-delete-old-logs.sh
+++ 12-delete-old-logs.sh
@@ -1,9 +1,11 @@
-SOURCE_DIR =/home/ec2-user/logs
+#!/bin/bash
 
-if [ -d SOURCE_DIR ]
-then
-    echo "Source directory exists: $SOURCE_DIR"
-else
-    echo "Source directory doesn't exists: $SOURCE_DIR"
-    exit 1
-fi
+SOURCE_DIR=/home/ec2-user/logs
+
+# if [ -d $SOURCE_DIR ]
+# then
+#     echo "Source directory exists: $SOURCE_DIR"
+# else
+#     echo "Source directory doesn't exists: $SOURCE_DIR"
+#     exit 1
+# fi
