e72015c473b87dcc02fdf5d687ec407056cb5960
siri123 <saisirishayarra692@gmail.com>
2024-09-03 18:22:47 +0530

    shell

---
+++ disk-usage.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+DISK_USAGE=df -hT | grep xfs
+DISK_THRESHOLD=5
+
+while IFS= read -r line
+do
+    disk_space=$line | awk -F " " '{print $6F}'
+    disk_folder=$line | awk -F " " 'print $NF'
+    if [ $disk_space -gt $DISK_THRESHOLD ]
+    then
+        echo "U have occupied $disk_space on folder $disk_folder .Go amd delete some folders"
+    fi
+
+
+
+done <<< $DISK_USAGE
