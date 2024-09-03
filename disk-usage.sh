7c213548d0088a4f9ad6d15c0dfe7e0d19750eee
siri123 <saisirishayarra692@gmail.com>
2024-09-03 18:31:29 +0530

    shell

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,8 +5,8 @@ DISK_THRESHOLD=5
 
 while IFS= read -r line
 do
-    disk_space=$line | awk -F " " '{print $6F}'
-    disk_folder=$line | awk -F " " 'print $NF'
+    disk_space=$($line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    disk_folder=$($line | awk -F " " '{print $NF}')
     if [ $disk_space -ge $DISK_THRESHOLD ]
     then
         echo "U have occupied $disk_space on folder $disk_folder .Go amd delete some folders"
