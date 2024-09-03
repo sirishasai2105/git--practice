f266710c4c1e46647e39ab95825d90038154e233
siri123 <saisirishayarra692@gmail.com>
2024-09-03 10:43:02 +0530

    shell

--- 05-special_variables.sh
+++ 05-special_variables.sh
@@ -6,4 +6,5 @@ echo "Getting the script name $0"
 echo "Printing current working directory $PWD"
 echo "Printing process instance id $$"
 echo  "current home directory is $HOME"
+sleep 100
 echo "process id of background command $!"
